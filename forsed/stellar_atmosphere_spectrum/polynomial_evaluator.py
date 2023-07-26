f78cfbda01b0bf988c9367b59ee3e535be397351
Alexa Villaume <avillaum@ucsc.edu>
2023-07-26 16:38:10 -0400

    recreating spi_utils functionality

--- forsed/stellar_atmosphere_spectrum/polynomial_evaluator.py
+++ forsed/stellar_atmosphere_spectrum/polynomial_evaluator.py
@@ -1,27 +1,64 @@
 
+import os 
+import glob
+from pathlib import Path
+
+
+
+import pandas as pd 
+
+import torch
 
 class PolynomialEvaluator(Stellar_Atmosphere_Spectrum):
 
     def __init__(self, coefficients):
 
-        pass
+
+        directory_path = Path().absolute()
+        data_path      = Path(directory_path.parent, 'data/Villaume2017a/')
+
+        self.polynomial_powers = {}
+        self.coefficients = {} 
+
+        for file_name in glob.glob(data_path + '*.dat'):
+
+            if 'polynomial_powers' in file_name:
+
+                with open(file_name, 'r') as f:
+                    self.polynomial_powers = eval(f.read)
+
+            else: 
+                coeffs = pd.read_csv(file_name, delim_whitespace=True, comment='#')
+                self.coefficients[os.path.split(file_name)[-1]] = torch.tensor(coeffs)
+
 
     def get_spectrum(self, stellar_type, surface_gravity, metalicity, effective_temperature) -> Tensor:
 
-        if stellar_type == "cool dwarf":
-            C = [[0,0,0], [1,0,0], [0,1,0], [0,0,1],[0,2,0],[2,0,0],[0,0,2], [1,1,0], [1,0,1], [0,1,1],[0,3,0],[3,0,0],[0,0,3],[2,1,0],[1,2,0],[2,0,1],[4,0,0],[0,4,0],[0,0,4],[2,2,0],[3,1,0], [5,0,0]]
-        elif stellar_type == "cool giants":
-            C = [[0,0,0], [1,0,0], [0,1,0], [0,0,1],[2,0,0],[0,0,2],[0,2,0], [0,1,1], [1,0,1], [1,1,0],[3,0,0],[0,0,3],[0,3,0],[1,1,1],[2,1,0],[2,0,1],[1,2,0],[0,2,1],[1,0,2],[0,1,2],[4,0,0]]
-        elif stellar_type == "warm dwarfs":
-            C = [[0,0,0], [1,0,0], [0,1,0], [0,0,1],[2,0,0],[0,0,2],[0,2,0], [1,1,0], [1,0,1],[3,0,0],[1,0,2],[0,3,0],[2,1,0],[2,0,1],[1,0,2],[1,1,1],[0,2,1],[4,0,0],[0,4,0],[3,1,0],[3,0,1],[2,2,0],[1,3,0],[2,0,2],[2,1,1],[5,0,0]]
-        elif stellar_type == "warm giants":
-            C = [[0,0,0], [1,0,0], [0,1,0], [0,0,1],[2,0,0],[0,0,2],[0,2,0], [1,1,0], [1,0,1], [0,1,1], [3,0,0], [0,0,3], [0,3,0], [2,1,0], [1,2,0], [2,0,1], [1,0,2], [4,0,0], [0,4,0],[2,2,0],[2,0,2],[0,2,2], [5,0,0]]
-        elif stellar_type == "hot stars":
-            C = [[0,0,0], [1,0,0], [0,1,0], [0,0,1],[2,0,0], [0,2,0],[0,0,2],[1,0,1], [1,1,0], [0,1,1], [3,0,0], [0,0,3],[0,3,0], [1,1,1], [2,1,0], [2,0,1], [0,2,1], [1,0,2], [0,1,2], [4,0,0]]
-        else:
-            raise ValueError(f"unknown stellar type: {stellar_type}")
+
+        # Overlap of cool dwarf and warm dwarf training sets
+        d_teff_overlap = np.linspace(3000, 5500, num=100)
+        d_weights = np.linspace(1, 0, num=100)
+
+        # Overlap of warm giant and hot star training sets
+        gh_teff_overlap = np.linspace(5500, 6500, num=100)
+        gh_weights = np.linspace(1, 0, num=100)
+
+        # Overlap of warm giant and cool giant training sets
+        gc_teff_overlap = np.linspace(3500, 4500, num=100)
+        gc_weights = np.linspace(1, 0, num=100)
+
+
+
+
+        # Normalizing to solar values
+        logt = np.log10(effective_temperature) - 3.7617
+        logg = surface_gravity - 4.44
+        feh = metalicity
+
+
+
         X = torch.stack(
-            tuple(effective_temperature**c[0] * metalicity**c[1] * surface_gravity**c[2] for c in C)
+            tuple(effective_temperature**c[0] * metalicity**c[1] * surface_gravity**c[2] for p in self.polynomial_powers)
         )
         spectrum = self.coefficients @ X
 
