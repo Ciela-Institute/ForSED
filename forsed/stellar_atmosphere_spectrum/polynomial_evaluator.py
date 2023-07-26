cf882f83e8422fcd58369205bf5b442fd6531ab0
Connor Stone <connorstone628@gmail.com>
2023-07-26 17:01:43 -0400

    Adding bounds for polyinterp

--- forsed/stellar_atmosphere_spectrum/polynomial_evaluator.py
+++ forsed/stellar_atmosphere_spectrum/polynomial_evaluator.py
@@ -8,58 +8,47 @@ from pathlib import Path
 import pandas as pd 
 
 import torch
+from stellar_atmosphere_spectrum import Stellar_Atmosphere_Spectrum
 
 class PolynomialEvaluator(Stellar_Atmosphere_Spectrum):
 
-    def __init__(self, coefficients):
+    def __init__(self):
 
 
         directory_path = Path().absolute()
         data_path      = Path(directory_path.parent, 'data/Villaume2017a/')
+        
+        self.coefficients = {}
 
-        self.polynomial_powers = {}
-        self.coefficients = {} 
-
-        for file_name in glob.glob(data_path + '*.dat'):
-
+        for file_name in glob.glob(str(data_path) + '/*.dat'):
             if 'polynomial_powers' in file_name:
-
                 with open(file_name, 'r') as f:
-                    self.polynomial_powers = eval(f.read)
-
-            else: 
+                    self.polynomial_powers = eval(f.read())
+            elif "bounds" in file_name:
+                with open(file_name, 'r') as f:
+                    self.bounds = eval(f.read())
+            else:
                 coeffs = pd.read_csv(file_name, delim_whitespace=True, comment='#')
-                self.coefficients[os.path.split(file_name)[-1]] = torch.tensor(coeffs)
+                self.coefficients[os.path.split(file_name)[-1][:-4]] = torch.tensor(coeffs.to_numpy())
 
 
-    def get_spectrum(self, stellar_type, surface_gravity, metalicity, effective_temperature) -> Tensor:
+    def get_spectrum(self, surface_gravity, metalicity, effective_temperature) -> torch.Tensor:
 
+        for key, ranges in self.bounds.items():
+            if ranges["surface_gravity"][0] < surface_gravity < ranges["surface_gravity"][1] and ranges["effective_temperature"][0] < effective_temperature < ranges["effective_temperature"][1]:
+                stellar_type = key
+                break
 
-        # Overlap of cool dwarf and warm dwarf training sets
-        d_teff_overlap = np.linspace(3000, 5500, num=100)
-        d_weights = np.linspace(1, 0, num=100)
-
-        # Overlap of warm giant and hot star training sets
-        gh_teff_overlap = np.linspace(5500, 6500, num=100)
-        gh_weights = np.linspace(1, 0, num=100)
-
-        # Overlap of warm giant and cool giant training sets
-        gc_teff_overlap = np.linspace(3500, 4500, num=100)
-        gc_weights = np.linspace(1, 0, num=100)
-
-
-
-
-        # Normalizing to solar values
-        logt = np.log10(effective_temperature) - 3.7617
-        logg = surface_gravity - 4.44
-        feh = metalicity
-
-
-
-        X = torch.stack(
-            tuple(effective_temperature**c[0] * metalicity**c[1] * surface_gravity**c[2] for p in self.polynomial_powers)
+        X = torch.tensor(
+            tuple(effective_temperature**c[0] * metalicity**c[1] * surface_gravity**c[2] for c in self.polynomial_powers[stellar_type])
         )
-        spectrum = self.coefficients @ X
+        spectrum = self.coefficients[stellar_type] @ X
 
         return spectrum
+
+if __name__ == "__main__":
+    P = PolynomialEvaluator()
+
+    import matplotlib.pyplot as plt
+    plt.plot(P.get_spectrum(3., 0., 4500))
+    plt.show()
--- forsed/stellar_atmosphere_spectrum/stellar_atmosphere_spectrum.py
+++ forsed/stellar_atmosphere_spectrum/stellar_atmosphere_spectrum.py
@@ -1,6 +1,7 @@
+from abc import ABC, abstractmethod
+from torch import Tensor
 
-@ABC
-class Stellar_Atmosphere_Spectrum():
+class Stellar_Atmosphere_Spectrum(ABC):
     @abstractmethod
     def get_spectrum(self, logg, Z, Teff) -> Tensor:
         pass
