b07eaec6cbc6aa70c1aef127360ed7b6586edd03
Connor Stone <connorstone628@gmail.com>
2023-07-26 14:09:35 -0400

    update SSP integrator

--- forsed/SSP/basic_ssp.py
+++ forsed/SSP/basic_ssp.py
@@ -3,24 +3,47 @@
 
 class Basic_SSP(Simulator):
 
-    def __init__(self, isochrone, imf, f_get_sasy):
+    def __init__(
+            self,
+            isochrone: "Isochrone",
+            imf: "Initial_Mass_Function",
+            sas: "Stellar_Atmosphere_Spectrum",
+    ):
 
         self.isochrone = isochrone
         self.imf = imf
-        self.f_get_sasy = f_get_sasy
+        self.sas = sas
 
     def forward(self, metalicity, Tage, alpha) -> Tensor:
 
         isochrone = self.isochrone.get_isochrone(metalicity, Tage)
 
         spectrum = torch.zeros(len)
-        for i in range(len(isochrone["phase"])):
-            elif isochrone["phase"][i] <= 2:
-                normalized_spectrum = self.f_get_sasy.get_spectrum(metalicity, Tage)
-                weight = self.imf.get_weight(isochrone["initial_mass"][i], alpha)
-                weight *= isochrone["initial_mass"][i] - isochrone["initial_mass"][i-1]
-                spectrum += normalized_spectrum * isochrone["luminosity"][i] * weight
-            elif isochrone["phase"][i] <= 5:
-                pass
-            else:
-                continue
+        
+        CHOOSE = isochrone["phase"] <= 2
+        spectrum += torch.trapz(
+            isochrone["initial_mass"][CHOOSE],
+            self.sas.get_spectrum(
+                isochrone["logg"][CHOOSE],
+                metalicity,
+                isochrone["Teff"][CHOOSE]
+            ) * self.imf.get_weight(
+                isochrone["initial_mass"][CHOOSE],
+                alpha
+            ) * isochrone["luminosity"][CHOOSE]
+        )
+            
+        CHOOSE = torch.logical_and(isochrone["phase"] > 2, isochrone["phase"] <= 5)
+        spectrum += torch.trapz(
+            isochrone["initial_mass"][CHOOSE],
+            self.sas.get_spectrum(
+                isochrone["logg"][CHOOSE],
+                metalicity,
+                isochrone["Teff"][CHOOSE]
+            ) * self.imf.get_weight(
+                isochrone["initial_mass"][CHOOSE],
+                alpha
+            ) * isochrone["luminosity"][CHOOSE]
+        )
+        
+        return spectrum
