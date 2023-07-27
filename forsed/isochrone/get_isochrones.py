e9740c6e18d1c7d8f901eca08d1233078680c939
Alexa Villaume <avillaum@ucsc.edu>
2023-07-27 11:22:59 -0400

    starting to add functionality of grabbing hte isochrones similar to what is currently in get_stellar_parameters.py...need to figure out how to decompress .txz files still! Just cheating on that for now

--- forsed/isochrone/get_isochrones.py
+++ forsed/isochrone/get_isochrones.py
@@ -2,5 +2,39 @@
 Methods to download isochrone libraries.
 """
 
-def get_mist_isochrones(library_name = "MIST_v1.2_vvcrit0.4_basic_isos.txz"):
-    pass
+
+import os
+from pathlib import Path
+
+
+def get_mist_isochrones(url='https://waps.cfa.harvard.edu/MIST/data/tarballs_v1.2/{}'):
+
+    import requests
+        
+    iso_version = 'MIST_v1.2_vvcrit0.4_basic_isos.txz'
+
+
+    directory_path = Path().absolute()
+    data_path      = Path(directory_path.parent, 'data/MIST/')
+    
+    try:
+        os.mkdir(data_path)
+    except Exception as e:
+        print(e)
+        pass
+
+    # check if file already exists
+    file_path = os.path.join(data_path, iso_version)
+
+    if not os.path.exists(file_path):
+        
+
+        r = requests.get(url.format(iso_version))
+
+        with open(file_path, 'w') as f:
+            f.write(r.text)
+
+
+if __name__=='__main__':
+
+    get_mist_isochrones()
