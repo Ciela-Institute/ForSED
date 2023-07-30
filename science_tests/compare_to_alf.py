75bf42a7897809b707fa8126e7dcd5a1583bd163
Alexa Villaume <avillaum@ucsc.edu>
2023-07-30 07:16:51 -0400

    comparison with kroupa imf working. differences in the two ssps that need to be folowed-up on.

--- science_tests/compare_to_alf.py
+++ science_tests/compare_to_alf.py
@@ -3,6 +3,7 @@ import sys
 import os
 import torch
 
+import numpy as np
 import matplotlib.pyplot as plt
 
 import pandas as pd
@@ -11,31 +12,39 @@ sys.path.insert(0, os.path.split(os.path.split(__file__)[0])[0])
 import forsed
 
 
-def grab_alf_ssp(): 
+def grab_alf_ssp(age=11.0, mcut=0.08, Z=+0.0): 
 
-    data_path = '/Users/alexa/ForSED/forsed/data/alf_data/'
-    fname     = 'VCJ_v8_mcut0.08_t11.0_Zp0.0.ssp.imf_varydoublex.s100'
+    # data_path = '/Users/alexa/ForSED/forsed/data/alf_data/'
+    # fname     = 'VCJ_v8_mcut0.08_t11.0_Zp0.0.ssp.imf_varydoublex.s100'
 
-    grid = pd.read_csv(data_path + fname, delim_whitespace=True)
+    # ssp_grid = pd.read_csv(data_path + fname, delim_whitespace=True)
 
-    wavelength = grid.iloc[:, 0]
-    ssp_grid   = grid.iloc[:, 1:]
+    fname = '/Users/alexa/Documents/Create_SSPs/CheckModels/vcj_cc8_Zp0.00_t13.5.ssp'
 
-    plt.plot(wavelength, ssp_grid.iloc[:,1])
-    plt.show()
+    ssp_grid = pd.read_csv(fname, delim_whitespace=True,
+                            header=0)
+
+
+    return ssp_grid
 
 if __name__=='__main__':
 
 
-    grab_alf_ssp()
+    alf_ssps = grab_alf_ssp()
 
-    # S = forsed.SSP.Basic_SSP(
-    #     forsed.isochrone.MIST(),
-    #     forsed.initial_mass_function.Kroupa(),
-    #     forsed.stellar_atmosphere_spectrum.PolynomialEvaluator(),
-    # )
+    S = forsed.SSP.Basic_SSP(
+        forsed.isochrone.MIST(),
+        forsed.initial_mass_function.Kroupa(),
+        forsed.stellar_atmosphere_spectrum.PolynomialEvaluator(),
+    )
 
-    # ssp = S.forward(torch.tensor(0., dtype = torch.float64), torch.tensor(10., dtype = torch.float64), torch.tensor([1.3, 2.3, 2.7]))
+    ssp = S.forward(torch.tensor(0., dtype = torch.float64), torch.tensor(13.5, dtype = torch.float64), torch.tensor([1.3, 2.3, 2.7]))
 
-    # plt.plot(S.sas.wavelength, ssp)
-    # plt.show()
+    plt.plot(alf_ssps['wavelength']*1e-4, alf_ssps['kroupa'], color='k')
+
+    plt.plot(S.sas.wavelength, ssp)
+    plt.show()
+
+
+    print(ssp.shape)
+    print(alf_ssps.shape)
