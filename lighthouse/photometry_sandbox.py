bdcd3b59ff9334ec088767d5766eb36b5e13ae35
Alexa Villaume <avillaum@ucsc.edu>
2023-08-22 10:06:40 -0400

    not sure at this point how to organize everything but just for the sake of getting started...

---
+++ lighthouse/photometry_sandbox.py
@@ -0,0 +1,14 @@
+import sedpy
+
+import unittest
+import sys
+import os
+import torch
+
+
+sys.path.insert(0, os.path.split(os.path.split(__file__)[0])[0])
+import lighthouse as lh
+
+if __name__=='__main__':
+
+    print("hello")
