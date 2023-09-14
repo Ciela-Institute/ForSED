36355a808626aaf74d51532fd7a53906e8044f2c
Alexa Villaume <avillaum@ucsc.edu>
2023-09-13 21:06:06 -0400

    the wonders of unit tests

--- tests/test_imf.py
+++ tests/test_imf.py
@@ -7,21 +7,42 @@ sys.path.insert(0, os.path.split(os.path.split(__file__)[0])[0])
 import lighthouse as lh
 
 
-class TestIMFWeights(unittest.TestCase):
+class TestIMF(unittest.TestCase):
 
-    def test_imf_weights(self):
+    def test_weights(self):
 
-        masses = torch.linspace(0.08, 100, 1000)
+        N = 100000
+        shift = (100 - 0.08)/N 
+        masses = torch.linspace(0.08 + shift/2, 100 - shift/2, N)
         dm = masses[1] - masses[0]
 
         for imf in lh.initial_mass_function.Initial_Mass_Function.__subclasses__():
 
             weights = imf().get_weight(masses)
 
-            self.assertAlmostEqual(torch.sum(weights*dm), 1, 3, "Check your IMF normalization!!!")
+            self.assertAlmostEqual(torch.sum(weights*dm).item(), 1, 3, "Check your IMF normalization!!!")
 
+    def test_continuity(self):
+
+        N = 1000000
+        shift = (100 - 0.08)/N 
+        masses = torch.linspace(0.08 + shift/2, 100 - shift/2, N)
+
+        for imf in lh.initial_mass_function.Initial_Mass_Function.__subclasses__():
+
+            print(imf)
+
+            weights = imf().get_weight(masses)
+
+            delta_weight = weights[1:] - weights[:-1]
+
+            print(delta_weight.abs()/weights[1:])
+
+            self.assertTrue(torch.all(delta_weight.abs()/weights[1:] < 1e-3), "Check your piecewise functions!!")
 
 class TestKroupa(unittest.TestCase):
+
+
     def test_kroupa(self):
         
 
