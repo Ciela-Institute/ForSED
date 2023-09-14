f3d4d8d3aa314cd7e641e6a4e7502ea366a3db2d
Alexa Villaume <avillaum@ucsc.edu>
2023-09-13 20:33:38 -0400

    adding unit test for imf normalization

--- tests/test_imf.py
+++ tests/test_imf.py
@@ -6,11 +6,27 @@ import torch
 sys.path.insert(0, os.path.split(os.path.split(__file__)[0])[0])
 import lighthouse as lh
 
+
+class TestIMFWeights(unittest.TestCase):
+
+    def test_imf_weights(self):
+
+        masses = torch.linspace(0.08, 100, 1000)
+        dm = masses[1] - masses[0]
+
+        for imf in lh.initial_mass_function.Initial_Mass_Function.__subclasses__():
+
+            weights = imf().get_weight(masses)
+
+            self.assertAlmostEqual(torch.sum(weights*dm), 1, 3, "Check your IMF normalization!!!")
+
+
 class TestKroupa(unittest.TestCase):
     def test_kroupa(self):
+        
 
         K = lh.initial_mass_function.Kroupa()
 
-        weight = K.get_weight(torch.tensor(30., dtype = torch.float64), torch.tensor([1.3, 2.3, 2.7]))
+        weight = K.get_imf(torch.tensor(30., dtype = torch.float64))
 
         self.assertAlmostEqual(weight.detach().cpu().numpy(), 5.1374e-5, 3, "Kroupa IMF test value not equal")
