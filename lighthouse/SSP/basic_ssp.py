dbf0ecd90d9be4d2a39cd4f8500d0b08dd3d211c
Alexa Villaume <avillaum@ucsc.edu>
2023-11-21 21:19:21 -0800

    checking full ssp synthesis in light of the bug fixes I made to the isochrone grid and isochrone selection...hopefully now I will have an easier time debugging the this now that the isochrones should at least be correct

--- lighthouse/SSP/basic_ssp.py
+++ lighthouse/SSP/basic_ssp.py
@@ -28,7 +28,7 @@ class Basic_SSP():
         self.highest_mass = None
         self.ms_turnoff_point = None
 
-    
+
     def get_ms_turnoff(self, ms_isochrone):
 
         turnoff = ms_isochrone["Teff"].argmax()
@@ -39,25 +39,25 @@ class Basic_SSP():
             if xx < 0.0:
                 turnoff = turnoff - 1
 
-        self.ms_turnoff_point = turnoff 
+        self.ms_turnoff_point = turnoff
 
 
 
-    @property 
-    def tage_normalization(self): 
-        # Normalizing to 1 solar mass at each epoch, 
+    @property
+    def tage_normalization(self):
+        # Normalizing to 1 solar mass at each epoch,
 
         if self._tage_normalization is None:
             self._tage_normalization = quad(self.imf.get_imf,
                         self.imf.lower_limit,
                         self.highest_mass,
                         args=(True,) )[0]
-            
+
         return self._tage_normalization
 
     def get_weight(self, mass, mass_weighted=True) -> torch.Tensor:
         return self.imf.get_imf(mass, mass_weighted=mass_weighted)/self.tage_normalization
-    
+
     def get_stellar_mass(self,  pop_masses) -> torch.Tensor:
         # pop_masses = isochrone["initial_mass"]
 
@@ -73,25 +73,25 @@ class Basic_SSP():
                 m2 = pop_masses[i]
             else:
                 m2 = pop_masses[i] + 0.5*(pop_masses[i+1] - pop_masses[i])
-            
+
             if m2 < m1:
                 print("IMF_WEIGHT WARNING: non-monotonic mass!", m1, m2, m2-m1)
-                continue 
-            
+                continue
+
             if m2 == m1:
                 print("m2 == m1")
                 continue
 
-            tmp, error =  quad(self.get_imf, 
-                                m1, 
-                                m2, 
+            tmp, error =  quad(self.get_imf,
+                                m1,
+                                m2,
                                 args=(False,) )
             weights.append(tmp)
 
         weights = np.asarray(weights)
 
         imf_weight = weights/self.t0_normalization
-    
+
         return imf_weight
 
 
@@ -117,7 +117,7 @@ class Basic_SSP():
         ms_spectra = torch.stack(tuple(
             self.sas.get_spectrum(tf, lg, metalicity,
             ) for lg, tf in zip(isochrone["log_g"][MS], isochrone["Teff"][MS])
-        )).T 
+        )).T
         ms_spectra *= 10**isochrone["log_l"][MS]
 
         hb_spectra = torch.stack(tuple(
@@ -134,19 +134,19 @@ class Basic_SSP():
         spectrum += torch.vmap(partial(torch.trapz, x = isochrone["initial_mass"][HB])) (
                         self.hb_weights*hb_spectra
                     )
-    
-        print(torch.sum(self.ms_weights * isochrone["current_mass"][MS]) + torch.sum(self.hb_weights * isochrone["current_mass"][HB]) ) 
-        print(torch.sum(self.ms_weights * isochrone["initial_mass"][MS]) + torch.sum(self.hb_weights * isochrone["initial_mass"][HB]) ) 
+
+        #print(torch.sum(self.ms_weights * isochrone["current_mass"][MS]) + torch.sum(self.hb_weights * isochrone["current_mass"][HB]) )
+        #print(torch.sum(self.ms_weights * isochrone["initial_mass"][MS]) + torch.sum(self.hb_weights * isochrone["initial_mass"][HB]) )
 
         summed_weights = self.ms_weights.sum().item() + self.hb_weights.sum().item()
-        print(summed_weights)
+        #print(summed_weights)
 
 
         # SSP in L_sun Hz^-1, CvD models in L_sun micron^-1, convert
         # spectrum *= utils.light_speed/self.sas.wavelength**2
 
         return spectrum
-    
+
 
 
 
