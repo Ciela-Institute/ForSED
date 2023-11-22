a3ae6f003cd88ca3026d1903e94baaa814a64f32
Alexa Villaume <avillaum@ucsc.edu>
2023-11-22 17:00:58 -0500

    using environment variable

--- lighthouse/isochrone/MIST_Isochrone.py
+++ lighthouse/isochrone/MIST_Isochrone.py
@@ -15,8 +15,7 @@ __all__ = ("MIST", )
 class MIST(Isochrone):
 
     def __init__(self, iso_file = 'MIST_v1.2_vvcrit0.0_basic_isos.hdf5'):
-        directory_path = Path(__file__).parent
-        data_path      = Path(directory_path.parent, 'data/MIST/')
+        data_path      = Path(os.environ['LightHouse_HOME'], 'data/MIST/')
 
         with h5py.File(os.path.join(data_path, iso_file), 'r') as f:
             self.isochrone_grid = torch.tensor(f["isochrone_grid"][:], dtype = torch.float64)
